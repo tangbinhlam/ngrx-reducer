b8bd6aa034c254fea82f4ccb8428eddef7379a06
osalam <tangbinhlam@gmail.com>
2020-08-19 01:47:59 -0500

    [ngrx] setup shopping List Reduce and Action and import StoreModule.forRoot with shoppingListReducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core.module';
-import { LoggingService } from './logging.service';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core.module";
+import { LoggingService } from "./logging.service";
+import { StoreModule } from "@ngrx/store";
+import { shoppingListReducer } from "./shopping-list/store/shopping-list.reducer";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -16,7 +18,8 @@ import { LoggingService } from './logging.service';
     HttpClientModule,
     AppRoutingModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forRoot({ shoppingList: shoppingListReducer }),
   ],
   bootstrap: [AppComponent],
   // providers: [LoggingService]
---
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,18 @@
+import { Ingredient } from "../../shared/ingredient.model";
+import * as ShoppingListAction from "./shopping-list.actions";
+
+const initialState = {
+  ingredients: [new Ingredient("Apples", 5), new Ingredient("Tomatoes", 10)],
+};
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListAction.AddIngredient
+) {
+  switch (action.type) {
+    case ShoppingListAction.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload],
+      };
+  }
+}
